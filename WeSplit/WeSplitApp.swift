54e52cc60fe81b2d4af0326a7f1fcce6a853208d
Valcasara Bryan <31510683+ValcasaraBryan@users.noreply.github.com>
2024-05-15 17:46:50 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by valcasara-bryan on 15/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
